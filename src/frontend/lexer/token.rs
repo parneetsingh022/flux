6503fb67866293b752f06dc626bb793b2f4b15ba
parneetsingh022 <parneetsingh022@gmail.com>
2026-02-25 17:55:12 -0800

    Implement lexer and parser structure with tokenization and AST generation

---
+++ src/frontend/lexer/token.rs
@@ -0,0 +1,52 @@
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType{
+    IntLiteral(i32),
+    FloatLiteral(f64),
+    StringLiteral(String),
+    CharLiteral(char),
+
+    Identifier(String),
+
+
+    // Keywords
+    Let,
+
+    // Punctuation
+    Semicolon,  // ;
+
+
+    // Operators
+    Equal,     // =
+    EqualEqual, // ==
+
+
+    // Others
+    Eof,
+}
+
+impl TokenType{
+    pub fn from_keyword(id : &str) -> Option<TokenType>{
+        match id { 
+            "let" => Some(TokenType::Let),
+            _ => None
+        }
+    }
+}
+
+
+
+
+/// Location of a token in a file
+#[derive(Debug, Clone, PartialEq)]
+pub struct Location {
+    pub line   : usize,
+    pub column : usize,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token{
+    pub token : TokenType,
+    pub location : Location
+}
